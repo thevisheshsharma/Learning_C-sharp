cffda24da62f66365d2cce669f6d4662e56c5033
Vishesh Sharma <visheshsharma1997@gmail.com>
2016-10-02 08:35:05 +0530

    Learnt args passing
    Args are always passed in a method in inc order.. {0}, {1}, {2}.. but
    can be used in any order...

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,13 +10,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World");
-            Console.WriteLine("who is there");
-            string firstname=Console.ReadLine();
-            string lastname=Console.ReadLine();
+            /* 
+             Console.WriteLine("Hello World");
+             Console.WriteLine("who is there");
+             string firstname=Console.ReadLine();
+             string lastname=Console.ReadLine();
 
-            Console.WriteLine("Hey " + firstname + " " + lastname);
+             Console.WriteLine("Hey " + firstname + " " + lastname);
+             Console.ReadKey();
+             */
+
+             //Program for learning Argument passing..
+            int userValue = 2;
+            string message = (userValue == 2) ? "boat" : "car";
+            string vishesh = "me";
+            Console.WriteLine("{2}-{1}-{0}",message,userValue,vishesh); //args are passed in inc order.. O,1,2..
+                                                                      //can be used in any ordr. but passed in inc.
             Console.ReadKey();
         }
     }
