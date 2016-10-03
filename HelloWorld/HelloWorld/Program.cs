74eff300be956fe4a757d278921ea6f3f0e88c53
Vishesh Sharma <visheshsharma1997@gmail.com>
2016-10-04 00:07:12 +0530

    Learnt if-else, for loop, break

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -20,13 +20,60 @@ namespace HelloWorld
              Console.ReadKey();
              */
 
-             //Program for learning Argument passing..
-            int userValue = 2;
-            string message = (userValue == 2) ? "boat" : "car";
-            string vishesh = "me";
-            Console.WriteLine("{2}-{1}-{0}",message,userValue,vishesh); //args are passed in inc order.. O,1,2..
-                                                                      //can be used in any ordr. but passed in inc.
-            Console.ReadKey();
+            //Program for learning Argument passing..
+            /*
+           int userValue = 2;
+           string message = (userValue == 2) ? "boat" : "car";
+           string vishesh = "me";
+           Console.WriteLine("{2}-{1}-{0}",message,userValue,vishesh); //args are passed in inc order.. O,1,2..
+                                                                     //can be used in any ordr. but passed in inc.
+           Console.ReadKey();
+           */
+
+
+            //Program to learn if-else, for loop, break..
+
+            Console.WriteLine("Enter string a::::  ");
+            string a =Console.ReadLine();
+            Console.WriteLine("Enter string b::::  ");
+            string b= Console.ReadLine();
+
+            int compareDiff = a.Length - b.Length;
+            if (compareDiff < 0)
+            {
+                compareDiff = -compareDiff;
+            }
+
+            if (a.Length < b.Length)
+            {
+                Console.WriteLine("B is bigger by "+ compareDiff);
+            } 
+            else if(a.Length>b.Length)
+            {
+                Console.WriteLine("A is bigger by "+ compareDiff);
+            }
+            else
+            {
+                Console.WriteLine("Both are equal");
+            }
+
+
+            for (int i = 1; i <= compareDiff; i++)
+            {
+                Console.WriteLine("You have " + (compareDiff - i) + " strings to enter!:");
+                string DiffEnter = Console.ReadLine();
+                if (string.IsNullOrEmpty(DiffEnter) == true)
+                {
+                    Console.WriteLine("You didn't entered anything!.. BYE");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You just entered " + DiffEnter);
+                }
+            }
+            Console.WriteLine("Time to say BYE");
+            Console.ReadKey(); 
         }
     }
 }
