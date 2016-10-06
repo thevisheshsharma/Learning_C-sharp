c266abadd98256e537054754ecb2724ff5284384
Vishesh Sharma <visheshsharma1997@gmail.com>
2016-10-07 00:17:25 +0530

    first Way if doing this via Method

--- UnderstandingMethods/UnderstandingMethods/Program.cs
+++ UnderstandingMethods/UnderstandingMethods/Program.cs
@@ -9,53 +9,36 @@ namespace UnderstandingMethods
     class Program
     {
         static void Main(string[] args)
-        {
-                Console.WriteLine("The Name Game");
+        {Console.WriteLine("The Name Game");
 
-                Console.Write("What's your first name? ");
-                string firstName = Console.ReadLine();
+            Console.Write("What's your first name? ");
+            string firstName = Console.ReadLine();
 
-                Console.Write("What's your last name? ");
-                string lastName = Console.ReadLine();
+            Console.Write("What's your last name? ");
+            string lastName = Console.ReadLine();
 
-                Console.Write("In what city were you born?");
-                string city = Console.ReadLine();
+            Console.Write("In what city were you born?");
+            string city = Console.ReadLine();
+            
+            Console.WriteLine("Results: ");
+            toArrRev(firstName);
+            toArrRev(lastName);
+            toArrRev(city);
 
-                char[] firstNameArray = firstName.ToCharArray();
-                Array.Reverse(firstNameArray);
-
-                char[] lastNameArray = lastName.ToCharArray();
-                Array.Reverse(lastNameArray);
-
-                char[] cityArray = city.ToCharArray();
-                Array.Reverse(cityArray);
-
-                string result = "";
-
-                foreach (char item in firstNameArray)
-                {
-                    result += item;
-                }
-
-                result += " ";
-
-                foreach (char item in lastNameArray)
-                {
-                    result += item;
-                }
-
-                result += " ";
-
-                foreach (char item in cityArray)
-                {
-                    result += item;
-                }
-
-                Console.WriteLine("Results: " + result);
-
-                Console.ReadLine();
+            Console.ReadLine();
 
             
+        }
+
+        static void toArrRev(string a)
+        {
+            char[] aArr = a.ToCharArray();
+            Array.Reverse(aArr);
+            foreach (char aArrItem in aArr)
+            {
+                Console.Write(aArrItem);
+            }
+            Console.Write(" ");
         }
     }
 }
