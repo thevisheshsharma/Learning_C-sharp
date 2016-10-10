1a4e9af14511fa551e44c1c3542b0b39e696a850
Vishesh Sharma <visheshsharma1997@gmail.com>
2016-10-10 23:44:07 +0530

    Date Tutorial
    Implemented DateTime data type, including
    1. formattting the date and time with functions, such as ToString,
    ToShortDate, ToLongDate
    2. extracting Month, Date, DayOfWeek, DayOfYear
    with the respective functions.
    3. Analysing and operating with Add and it's sub-functions.
    4. Using TimeSpan data type.

---
+++ DateTutorial/DateTutorial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime dt = DateTime.Now;
+            //Console.WriteLine(dt);
+            //Console.WriteLine(dt.ToString());
+            //Console.WriteLine(dt.ToLongDateString());
+            //Console.WriteLine(dt.ToShortDateString());
+            //Console.WriteLine(dt.ToLongTimeString());
+            //Console.WriteLine(dt.ToShortTimeString());
+            //Console.WriteLine(dt.Ticks);
+            //Console.WriteLine(dt.AddTicks(3).Ticks);
+            //Console.WriteLine(dt.AddDays(3));
+
+
+            //DateTime viafunc = new DateTime(2000, 10, 20);
+            //Console.WriteLine(viafunc);
+
+            //DateTime viaParse = DateTime.Parse("06-07-08");
+            //Console.WriteLine(viaParse.Date);
+
+            DateTime mybday = DateTime.Parse("12-07-1969");
+            TimeSpan age = DateTime.Now.Subtract(mybday);
+            Console.WriteLine(age.Days/365);
+        }
+    }
+}
